830f54b5e7353b3db4579816788742c5487fc804
Faith Wairimu <fwairimu967@gmail.com>
2025-03-19 13:35:07 +0300

    Add files via upload

---
+++ Akili Mingi/java.js
@@ -0,0 +1,10 @@
+<html>
+	<head>
+	<script> </script> <!This is used to save external js file..>
+	</head>
+	<body
+	<script language="javascript" type="text/javascript">
+		document.write ("Learning Javascript!")
+	</script>
+	</body>
+</html>
